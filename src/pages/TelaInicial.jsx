acd2a0911fb2105df9f7523c78cc6adb9330d14b
Théo Barreto Silva <barretotheo25@gmail.com>
2022-10-17 19:54:51 -0300

    upload

--- src/pages/TelaInicial.jsx
+++ src/pages/TelaInicial.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom"
+
 export const TelaInicial = () => {
-    return <h1>Tela inicial</h1>
+    return(
+        <>
+            <h1>Tela inicial</h1>
+            <Link to={"/biotipos"}>Ir tela de biotipos</Link>
+        </>
+    ) 
 }
