92c26a2ac152d93c88820c1f01b5a46f529a7d99
Théo Barreto Silva <barretotheo25@gmail.com>
2022-11-16 09:09:38 -0300

    continuando pagina inicial

---
+++ src/components/ImagemInicio/ImagemInicio.jsx
@@ -0,0 +1,9 @@
+import { DivImgStyled, ImagemStyled } from "./ImagemInicio.styles";
+
+export const ImagemInicio = () => {
+    return(
+        <DivImgStyled>
+            <ImagemStyled src=""/>
+        </DivImgStyled>
+    );
+};
---
+++ src/components/ImagemInicio/ImagemInicio.styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const DivImgStyled = styled.div`
+    width: 100;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+export const ImagemStyled = styled.img`
+    width: 100%;
+`
