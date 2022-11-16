0905e2200dc5996b1bb9e76c1349a623fcd24e76
Théo Barreto Silva <barretotheo25@gmail.com>
2022-11-16 11:02:41 -0300

    upload

--- src/components/ImagemInicio/ImagemInicio.styles.jsx
+++ src/components/ImagemInicio/ImagemInicio.styles.jsx
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 
 export const DivImgStyled = styled.div`
-    width: 100;
-    position: fixed;
-    left: 0;
-    right: 0;
-`
-
-export const ImagemStyled = styled.img`
     width: 100%;
+    height: 70vh;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    display: flex;
+    position: absolute;
+    justify-content: center;
+    justify-items: center;
+    background-size: 100%;
+    background-image: url(https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/imagemInicial.png?raw=true);
 `
 
 export const NomeLogoStyled = styled.h1`
     font-family: 'Montserrat', sans-serif;
-    font-size: 50px;
+    font-size: 90px;
     color: #033439;
+    font-weight: 500;
+    margin: 0;
+    text-align: center;
 `
