6d226a8ab1b3b0a60d7f787f976914782061cca0
Théo Barreto Silva <barretotheo25@gmail.com>
2023-07-10 17:10:16 -0300

    upload

--- src/components/TesteBiotipo/TesteBiotipo.jsx
+++ src/components/TesteBiotipo/TesteBiotipo.jsx
@@ -1,4 +1,4 @@
-import { P2Styled } from "../TextoBase/TextoBase.styles"
+import { TextoBase } from "../TextoBase/TextoBase.styles"
 import { SubtituloStyled } from "../Subtitulo/Subtitulo.styles"
 import { DivGeralStyled, DivTesteIndividualStyled, ImgTesteStyled } from "./TesteBiotipo.styles"
 
@@ -7,17 +7,17 @@ export const TesteBiotipo = () => {
         <DivGeralStyled>
             <DivTesteIndividualStyled>
                 <SubtituloStyled>Endomorfo</SubtituloStyled>
-                <P2Styled>Não consegue tocar os<br />dedos</P2Styled>
+                <TextoBase>Não consegue tocar os<br />dedos</TextoBase>
                 <ImgTesteStyled src="https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/testeEndomorfo.png?raw=true"/>
             </DivTesteIndividualStyled>
             <DivTesteIndividualStyled>
                 <SubtituloStyled>Mesomorfo</SubtituloStyled>
-                <P2Styled>Encontra a ponta dos<br />dedos</P2Styled>
+                <TextoBase>Encontra a ponta dos<br />dedos</TextoBase>
                 <ImgTesteStyled src="https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/testeMesomorfo.png?raw=true"/>
             </DivTesteIndividualStyled>
             <DivTesteIndividualStyled>
                 <SubtituloStyled>Ectomorfo</SubtituloStyled>
-                <P2Styled>Sobreposição de um<br />dedo no outro</P2Styled>
+                <TextoBase>Sobreposição de um<br />dedo no outro</TextoBase>
                 <ImgTesteStyled src="https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/testeEctomorfo.png?raw=true"/>
             </DivTesteIndividualStyled>
         </DivGeralStyled>
