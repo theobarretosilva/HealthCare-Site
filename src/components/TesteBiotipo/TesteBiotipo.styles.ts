21cb32b20ccad758429f4ac92afb707afc67c149
Théo Barreto Silva <barretotheo25@gmail.com>
2023-07-11 11:55:28 -0300

    upload

--- src/components/TesteBiotipo/TesteBiotipo.styles.ts
+++ src/components/TesteBiotipo/TesteBiotipo.styles.ts
@@ -1,20 +1,11 @@
 import styled from "styled-components";
 
 export const DivGeralStyled = styled.div`
-    @media (min-width: 650px){
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        margin: 4vh 5vw 4vh 5vw;
-    }
-
-    @media (max-width: 650px){
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        margin: 4vh 5vw 4vh 5vw;
-    }
-    
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 4vh 5vw 4vh 5vw;
 `
 
 export const DivTesteIndividualStyled = styled.div`
