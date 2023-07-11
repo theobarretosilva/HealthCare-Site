5705a98e032f50071eb947fe6e4701276559d2c1
Théo Barreto Silva <barretotheo25@gmail.com>
2023-07-11 10:43:50 -0300

    upload

--- src/components/SubDiv/SubDiv.styles.ts
+++ src/components/SubDiv/SubDiv.styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const SubDiv = styled.div`
-    margin-top: 4%;
+    margin-top: 5vh;
 `
--- src/pages/TelaBiotipos/TelaBiotipos.tsx
+++ src/pages/TelaBiotipos/TelaBiotipos.tsx
@@ -1,5 +1,4 @@
 import { DivCentralizadora } from '../../components/DivCentralizadora/DivCentralizadora.styles';
-import { Div2Styled } from '../../components/Div2/Div2.styles';
 import { SubDiv } from '../../components/SubDiv/SubDiv.styles';
 import { Header } from '../../components/Header/Header'
 import { ImgStyled } from '../../components/Imagens/Imagens.styles';
@@ -21,44 +20,44 @@ export const TelaBiotipos = () => {
                 <TextoBase>Abaixo você encontrará dicas para descobrir o seu.</TextoBase>
                 <hr />
                 <SubDiv>
-                    <SubtituloStyled>Ectomorfo</SubtituloStyled>
+                    <SubtituloStyled style={{ color: '#063E44' }}>Ectomorfo</SubtituloStyled>
                     <TextoBase>
                         As pessoas com o biotipo corporal ectomorfo apresentam corpo mais magro e esguio, ombros estreitos e membros compridos. 
                         Além disso, as mulheres apresentam quadril mais estreito e seios com  pouco músculo e pouca gordura.
                     </TextoBase>
-                    <Div2Styled>
+                    <S.CentralizaImg>
                         <ImgStyled src='https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/ectomorfo.png?raw=true'/>
-                    </Div2Styled>
+                    </S.CentralizaImg>
                     <TextoBase>
                         Além de apresentar um metabolismo mais rápido e, por isso, são capazes de perder peso mais facilmente, mas têm mais
                         dificuldade para ganhar massa muscular.
                     </TextoBase>
                 </SubDiv>
                 <SubDiv>
-                    <SubtituloStyled>Endomorfo</SubtituloStyled>
+                    <SubtituloStyled style={{ color: '#063E44' }}>Endomorfo</SubtituloStyled>
                     <TextoBase>
                         As pessoas com biotipo corporal endomorfo apresentam baixa estatura, corpo mais arredondado e mais largo, de forma
                         que as mulheres com esse biotipo apresentam quadril mais largo, associado ao formato de uma pera, e maior quantidade
                         de gordura acumulada.
                     </TextoBase>
-                    <Div2Styled>
+                    <S.CentralizaImg>
                         <ImgStyled src='https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/endomorfo.png?raw=true'/>
-                    </Div2Styled>
+                    </S.CentralizaImg>
                     <TextoBase>
                         Além de apresentarem metabolismo mais lento, maior acúmulo de gordura e têm mais dificuldade para perder peso,
                         apesar de maior facilidade para ganhar massa muscular.
                     </TextoBase>
                 </SubDiv>
                 <SubDiv>
-                    <SubtituloStyled>Mesomorfo</SubtituloStyled>
+                    <SubtituloStyled style={{ color: '#063E44' }}>Mesomorfo</SubtituloStyled>
                     <TextoBase>
                         As pessoas com biotipo corporal mesomorfo possuem o corpo magro e musculoso, mesmo que não pratiquem atividade
                         física com frequência. Além disso, as pessoas com esse biotipo possuem o tronco mais desenvolvido, pouca gordura
                         abdominal, cintura mais fina e metabolismo mais rápido, além de ganhar massa muscular com facilidade.
                     </TextoBase>
-                    <Div2Styled>
+                    <S.CentralizaImg>
                         <ImgStyled src='https://github.com/theobarretosilva/HealthCare-Site/blob/master/src/assets/imgs/mesomorfo.png?raw=true'/>
-                    </Div2Styled>
+                    </S.CentralizaImg>
                     <TextoBase>
                         A pessoa com características mesomórficas, também pode ganhar gordura corporal. Por isso, o consumo de açúcar refinado,
                         de alimentos industrializados, e alimentos gordurosos.
